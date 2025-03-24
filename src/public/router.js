8701db8e58291213296036080dcb57837ba7f59b
ghlee <94736572+neo622@users.noreply.github.com>
2025-03-24 21:46:50 +0900

    fix: innerhtml 루트로 변경

--- src/public/router.js
+++ src/public/router.js
@@ -5,41 +5,42 @@ import { ErrorPage } from "../components/ErrorPage.js";
 import { getUser, removeUser } from "../common/store.js";
 
 export const routes = {
-    "/": MainPage,
-    "/login": LoginPage,
-    "/profile": ProfilePage,
+  "/": MainPage,
+  "/login": LoginPage,
+  "/profile": ProfilePage,
 };
 
 export const Router = {
-    render: () => {
-        const currentUser = getUser();
-        let path = window.location.pathname;
-        const page = routes[path] || ErrorPage;
-        document.body.innerHTML = page.render();
+  render: () => {
+    const root = document.getElementById("root");
+    const currentUser = getUser();
+    let path = window.location.pathname;
+    const page = routes[path] || ErrorPage;
+    root.innerHTML = page.render();
 
-        if (page.afterRender) {
-            page.afterRender();
-        }
-        if (path === "/profile" && !currentUser) {
-            history.replaceState(null, "", "/login");
-            Router.render();
-        }
+    if (page.afterRender) {
+      page.afterRender();
+    }
+    if (path === "/profile" && !currentUser) {
+      history.replaceState(null, "", "/login");
+      Router.render();
+    }
 
-        document.querySelectorAll("a").forEach((a) => {
-            a.addEventListener("click", (e) => {
-                e.preventDefault();
-                console.log(e.target.id);
-                if (e.target.id === "logout") {
-                    removeUser();
-                };
-                const targetPath = a.getAttribute("href");
-                history.pushState(null, "", targetPath);
-                Router.render();
-            });
-        });
-    },
-    navigate: (path) => {
-        history.pushState(null, "", path);
+    document.querySelectorAll("a").forEach((a) => {
+      a.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log(e.target.id);
+        if (e.target.id === "logout") {
+          removeUser();
+        }
+        const targetPath = a.getAttribute("href");
+        history.pushState(null, "", targetPath);
         Router.render();
-    },
+      });
+    });
+  },
+  navigate: (path) => {
+    history.pushState(null, "", path);
+    Router.render();
+  },
 };
